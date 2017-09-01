49535f19675907bd3ac23b80663887e6be932512
Zhang Yu <octopusthu@gmail.com>
2017-09-01 10:39:19 +0800

    Avoids startup failure when no message source is found.

--- ejw-core/src/main/java/com/octopusthu/ejw/component/ExposedResourceBundleMessageSource.java
+++ ejw-core/src/main/java/com/octopusthu/ejw/component/ExposedResourceBundleMessageSource.java
@@ -1,31 +1,36 @@
 package com.octopusthu.ejw.component;
 
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.ResourceBundle;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.*;
 
 public class ExposedResourceBundleMessageSource extends ResourceBundleMessageSource {
 
-	public Map<String, String> getAllMessages(Locale locale) {
-		Map<String, String> ret = new HashMap<String, String>();
-		super.getBasenameSet().forEach(basename -> {
-			ret.putAll(this.getAllMessages(basename, locale));
-		});
-		return ret;
-	}
+    public Map<String, String> getAllMessages(Locale locale) {
+        Map<String, String> ret = new HashMap<String, String>();
+        Set<String> basenameSet = super.getBasenameSet();
+        if (CollectionUtils.isEmpty(basenameSet)) {
+            return ret;
+        }
+        basenameSet.forEach(basename -> {
+            ret.putAll(this.getAllMessages(basename, locale));
+        });
+        return ret;
+    }
 
-	public Map<String, String> getAllMessages(String basename, Locale locale) {
-		Map<String, String> ret = new HashMap<String, String>();
-		ResourceBundle resourceBundle = super.getResourceBundle(basename, locale);
-		Enumeration<String> e = resourceBundle.getKeys();
-		while (e.hasMoreElements()) {
-			String key = e.nextElement();
-			ret.put(key, resourceBundle.getString(key));
-		}
-		return ret;
-	}
+    public Map<String, String> getAllMessages(String basename, Locale locale) {
+        Map<String, String> ret = new HashMap<String, String>();
+        ResourceBundle resourceBundle = super.getResourceBundle(basename, locale);
+        if (resourceBundle == null) {
+            return ret;
+        }
+        Enumeration<String> e = resourceBundle.getKeys();
+        while (e.hasMoreElements()) {
+            String key = e.nextElement();
+            ret.put(key, resourceBundle.getString(key));
+        }
+        return ret;
+    }
 
 }
